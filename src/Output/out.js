a2c7ce09de57247f5f27f85e82e7ac58e325c569
QuantumImmortality <lorenzo.monsif1@gmail.com>
2019-04-15 18:17:57 +1000

    Can receive message and respond

---
+++ src/Output/out.js
@@ -0,0 +1,8 @@
+async function sendMessage(client, message){
+    var botChat = client.channels.get("406961766250053653")
+    botChat.send(message)
+}
+
+module.exports = {
+    sendMessage: sendMessage
+}
--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,10 @@
 const auth = require('../config/credentials.json');
+const commandPrefix = require('../config/config.json').commandPrefix
+const output = require('./Output/out.js')
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
+client.login(auth.token)
 
 client.on('ready', () => {
     // List servers the bot is connected to
@@ -14,4 +18,12 @@ client.on('ready', () => {
     })
 })
 
-client.login(auth.token)
+client.on('message', msg => {
+    if (msg.author.bot || !msg.content.startsWith(commandPrefix)) return;
+    let command = msg.content.substr(1)
+    output.sendMessage(client, "Message Received: " + command)
+    //let args = msg.content.slice(commandChar.length).trim().split(' ');
+    //let cmd = args.shift().toLowerCase();
+
+    //commandService.handle(client, msg, cmd, args);
+});
