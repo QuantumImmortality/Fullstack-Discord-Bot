f2896408b783ba433af95fd26f5a5bb0a1837465
root <root@mongodb.us-east1-b.c.ethereal-mind-238405.internal>
2019-06-04 01:27:49 +0000

    Working table that has expandable rows, showing either preview of image or youtube vid. Next is to add buttons for deletion and update on each row and a generall add button.

--- angularJS/src/app/components/list/list.component.ts
+++ angularJS/src/app/components/list/list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { IssueService } from '../../issue.service';
 
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { MatTableDataSource } from '@angular/material';
 import { Meme } from '../../meme.model';
 import { animate, state, style, transition, trigger } from '@angular/animations';
-
+//import { YT } from '../../youtubeFilter';
 
 @Component({
   selector: 'list.component',
@@ -14,7 +15,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   animations: [
     trigger('detailExpand', [
       state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
+      state('expanded', style({height: 'auto'})),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
@@ -25,10 +26,10 @@ export class ListComponent implements OnInit {
   memes: Meme[];
   columnsToDisplay = ['_id', 'name', 'source'];
 
-  //isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('source');
   expandedElement: Meme | null;
 
-  constructor(private issueService: IssueService, private router: Router) { }
+  constructor(private issueService: IssueService, private router: Router, private sanitizer: DomSanitizer) { }
+
 
   ngOnInit() {
     this.fetchMemes();
@@ -41,9 +42,31 @@ export class ListComponent implements OnInit {
       this.memes = data["data"];
       console.log('Data requested ... ');
       console.log(data["data"]);
-      //const rows = [];
-      //data.forEach(element => rows.push(element, { detailRow: true, element }));
-      //console.log(rows);
-    });
+      for(var i = 0; i < this.memes.length; i++) {
+	console.log(this.getId(this.memes[i].source));        
+      }	    
+   });
   }
+
+  sanitizeURL(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+          isolateId(url) {
+                var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+                var match = url.match(regExp);
+
+                if (match && match[2].length == 11) {
+                    return match[2];
+                } else {
+                    return 'error';
+                }
+            }
+
+            getId(url) {
+                return this.isYoutube(url) ? 'https://www.youtube.com/embed/' + this.isolateId(url) : url;
+            }
+	
+	   isYoutube(url){
+	        return url.includes("youtu");
+	   }
 }
